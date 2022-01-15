6e3d3da963cb042535be5c2ab800807202d111c4
charles onuorah <charles.onuorah@yahoo.com>
2022-01-15 17:35:24 +0100

    fix minue issue

--- server/controller/Robot.ts
+++ server/controller/Robot.ts
@@ -341,7 +341,7 @@ class Robot{
                 }, {recieved_messages: newState})
 
                 
-                await Currency.updateOne({pair: payload.pair}, {value: currency.value - payload.rating})
+                await Currency.updateOne({pair: payload.pair}, {value: currency.value + payload.rating})
                 
 
                 await DbLog.create({
@@ -387,7 +387,7 @@ class Robot{
                 }, {recieved_messages: newState})
 
                 
-                await Currency.updateOne({pair: payload.pair}, {value: currency.value - payload.rating})
+                await Currency.updateOne({pair: payload.pair}, {value: currency.value + payload.rating})
                 
 
                 await DbLog.create({
@@ -431,7 +431,7 @@ class Robot{
                 }, {recieved_messages: newState})
 
                 
-                await Currency.updateOne({pair: payload.pair}, {value: currency.value - payload.rating})
+                await Currency.updateOne({pair: payload.pair}, {value: currency.value + payload.rating})
                 
 
                 await DbLog.create({
@@ -475,7 +475,7 @@ class Robot{
                 }, {recieved_messages: newState})
 
                 
-                await Currency.updateOne({pair: payload.pair}, {value: currency.value - payload.rating})
+                await Currency.updateOne({pair: payload.pair}, {value: currency.value + payload.rating})
                 
 
                 await DbLog.create({
